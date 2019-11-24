6bba39d9db76e3a189858ebcc55c65697e57274f
Vishesh Goyal <heyvishesh@gmail.com>
2019-11-23 21:00:27 -0800

    Merge branch 'master' of https://github.com/VisheshKG/SkyStone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldConfiguration.java
@@ -9,8 +9,10 @@ package org.firstinspires.ftc.teamcode;
 public class fieldConfiguration {
     public static final boolean BLUESIDE =false;      //if red side, set it to false
     public static final boolean PARK_INSIDE =true;
-    public static final double robotStartX= 41;      // robot origin aline with image right
-    public static final double robotStartY=17.25;       //right back corner of robot
+    public static final boolean START_STONE_SIDE=true;  //true if start at stone side
+
+    public static double robotStartX= 41;      // robot origin aline with image right
+    public static double robotStartY=17.25;       //right back corner of robot
 
     public static final double delayParkingBySeconds = 0;
 
@@ -24,8 +26,24 @@ public class fieldConfiguration {
     public static final double bridgeY=46;
 
     //Robot measures
-    public static final double robotLength=23;
+    public static final double robotLength=22;
     public static final double robotWidth=17.25;
 
+    public static void initRobotStartX(){
+        if (START_STONE_SIDE) {
+            if (BLUESIDE) {
+                robotStartX = 41.5;   //blue stone side: align with wall image right edge
+            } else {
+                robotStartX = 24;   //red stone side: align with tile edge
+            }
+        }else{     //foundation side
+            if (BLUESIDE) {
+                robotStartX = -48;   //blue foundation side: align tile
+                robotStartY = 0;
+            } else {
+                robotStartX = -48;   //red foundation side: align with tile edge
+            }
+        }
+    }
 
 }
